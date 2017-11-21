c7503dfa2a69e72bfcd2efc0c1c38723e09348ce
tomoess <428558188@qq.com>
2017-11-21 16:25:59 +0800

    Update order_feature.py

--- order_feature.py
+++ order_feature.py
@@ -11,23 +11,31 @@ data_start = '2016-8'
 data_end = '2016-10'
 
 data_dir = 'data/'
-dest_dir = 'feature/'
+dest_dir = 'features/'
 
 order_data = pd.read_csv(data_dir+'t_order.csv')
+print('data loaded')
+
 order_data['buy_time'] = pd.to_datetime(order_data['buy_time']) #将数据类型转换为日期类型
 order_data = order_data.set_index('buy_time') # 将date设置为index
 order_data = order_data[data_start:data_end]
 
-#通过单价和数量算出总价
-order_data['account'] = Series([order_data['price'][x]*order_data['qty'][x]  for x in range(0,len(order_data))])
+
+print('figuring account...')
+order_data['account'] = order_data['price'] * order_data['qty']#通过单价和数量算出总价。
+#现在的价格没有可加性，后续应加上幂函数变换
+
+print('figuring groupby_sum...')
 grouped_uid_sum = order_data.groupby(by = ['uid'],as_index = False).sum()
-price_sum = grouped_uid_sum['account']
+account_sum = grouped_uid_sum['account']
 discount_sum = grouped_uid_sum['discount']
 
 #grouped_uid_cnt = order_data.groupby(by = ['uid'],as_index = False).count()
 order_features = pd.DataFrame()
-order_features['uid'] = order_data['uid']
-order_features['account_sum'] = price_sum
+order_features['uid'] = grouped_uid_sum['uid']
+order_features['account_sum'] = account_sum
 order_features['disaccount_sum'] = discount_sum
 
-order_features.to_csv(dest_dir+'order_features',index = False)
+print('saving...')
+order_features.to_csv(dest_dir+'order_features.csv',index = False)
+print('order_features.csv generated')
